5f2ebe604fab6f84f2cd1f426c75b1aa016bd48c
RoxiFOchoaG <46870050+roxifochoag@users.noreply.github.com>
2023-08-16 10:39:21 -0500

    Actualización de la información

---
+++ bin/md-links.js
@@ -0,0 +1,9 @@
+const main=(path)=>{
+
+  if (path){
+    return true;
+  }
+    return false;
+}
+
+module.exports ={main};
---
+++ cli.js
@@ -0,0 +1,3 @@
+const mdLinks = require('./bin/md-links.js');
+const response = mdLinks.main('prueba');
+console.log('Prueba uno',response);
