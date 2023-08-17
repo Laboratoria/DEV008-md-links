33589e606a06712cd771c54e23c849bc8e2172a5
Dani-dan99 <ldcpdan@gmail.com>
2023-08-17 15:22:03 -0600

    add structure of files

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
+//promesa-md-links
+const fs = require("fs");
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(path)) {
+    } else {
+      reject("The route doen't exist.");
+    }
+  });
 };
+
+module.exports = { mdLinks };
