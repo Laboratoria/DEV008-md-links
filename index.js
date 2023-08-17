05b238cfbe896cfbfe685dcb3018476444cde206
Danitap-97 <131627619+Danitap-97@users.noreply.github.com>
2023-08-17 17:25:28 -0500

    instale dependencias

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 //aqui registro funciones de md-link
+const fs =require('fs').promises;
+const path = require('path');
+//const{
+  //aqui van las funciones
+//}=require(./function.js)
+
+
 module.exports = () => {
   // ...
 };
