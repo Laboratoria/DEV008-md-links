af871a28b22b03efdcb84a329407070e11c0eb34
Kimberly Rv <elliis.rv@gmail.com>
2023-08-23 00:24:19 -0600

    verify path, absolute path and convert to absolute path functions

--- functions.js
+++ functions.js
@@ -11,7 +11,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const examplePath = 'README.md'; //Debe ser un string vacío/ Se llena para pruebas aquí
+const examplePath = 'example.md'; //Debe ser un string vacío/ Se llena para pruebas aquí // Con filename si lo valida correctamente
 
 // 1)
 function validatePath(userPath) { //Solo aceota rutas relativas **SOLUCIONAR**
@@ -27,45 +27,18 @@ function validatePath(userPath) { //Solo aceota rutas relativas **SOLUCIONAR**
 }
 validatePath(examplePath);
 
-/*if (validatePath(userPath)) {
-    //validar ruta ABOLUTA
-  console.log('Valid path');
-} else {
-  console.log('Invalid path');
-};*/
-
-
 // 2)
-/*function validateAbsolutePath(path) {
-  path.isAbsolute(path);
-if (validateAbsolutePath(path)) {
-    console.log(path);
-    console.log('Valid absolute path')
-    return true
-  } else {
-    console.log('Invalid absolute path');
-  }
-};
-validateAbsolutePath(examplePath);*/
-
-/*function validateAbsolutePath(userPath) {
-    const validatedPath = validatePath(userPath); 
-    const validatedAbolutePath = path.isAbsolute(userPath);
-
-    if (validatedPath === true && validatedAbolutePath === true ) {
-        console.log(validatedAbolutePath)   
-        console.log('Valid absolute path') 
-        return validatedAbolutePath
-    } else {
-        //Convertir en ruta absoluta
-        console.log('Invalid absolute path');
-    };
-};
-validateAbsolutePath(examplePath);*/
-
 function validateAbsolutePath(userPath) {
     const validatedAbsolutePath = path.isAbsolute(userPath);
     console.log('Absolute Path is ' + validatedAbsolutePath)
     return validatedAbsolutePath;
 };
-//validateAbsolutePath(examplePath);
+
+// 3)
+function convertToAbsolutePath(relativePath) {
+    const absolutePath = path.resolve(relativePath);
+    console.log('Absolute path:', absolutePath);
+  return absolutePath
+}
+convertToAbsolutePath(examplePath);
+
