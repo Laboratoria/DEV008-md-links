1234c887d0428069a76321ae25eb4451561b6b41
Kimberly Rv <elliis.rv@gmail.com>
2023-08-24 13:09:21 -0600

    validate if is a file, identify file extension, read the file and find the links in the file.md

--- functions.js
+++ functions.js
@@ -4,8 +4,17 @@
 
 // 2) Si es válida (T) -----> Validar ruta absoluta -----> T/F
 
-// 3) Si no es absoluta (F) -----> convetir
+// 3) Si no es absoluta (F) -----> convetir 
+                    
+// 4) Identificar si es archivo o directorio -----> trabajar con archivo
 
+// 5) Archivo -----> identificar extensión .md
+
+// 6) Leer el archivo
+
+// 7) Identificar links
+
+                    //**COMPLETADO**//
 
 
 const path = require('path');
@@ -14,7 +23,7 @@ const fs = require('fs');
 const examplePath = 'example.md'; //Debe ser un string vacío/ Se llena para pruebas aquí // Con filename si lo valida correctamente
 
 // 1)
-function validatePath(userPath) { //Solo aceota rutas relativas **SOLUCIONAR**
+function validatePath(userPath) { //Solo acepta rutas relativas **SOLUCIONAR** // Con filename o direname si lo valida correctamente
     try {
         fs.accessSync(userPath);
         console.log('Valid path')
@@ -34,11 +43,59 @@ function validateAbsolutePath(userPath) {
     return validatedAbsolutePath;
 };
 
+    //TRATAR DE FUSIONAR VALIDAR Y CONVERTIR CON IF//
+
 // 3)
 function convertToAbsolutePath(relativePath) {
     const absolutePath = path.resolve(relativePath);
     console.log('Absolute path:', absolutePath);
   return absolutePath
-}
-convertToAbsolutePath(examplePath);
+};
+const convertedAbsolutePath = convertToAbsolutePath(examplePath);
+//console.log(convertedAbsolutePath);
 
+//4)
+function identifyFile(filePath) {
+    try {
+      const stats = fs.statSync(filePath);
+      if (stats.isFile()) {
+        //console.log(stats);
+        console.log(`${filePath} is a file.`);
+      } else {
+        console.log(`${filePath} is not a file.`);
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+identifyFile(convertedAbsolutePath);
+
+// 5)
+
+function identifyFileExtension(filePath) {
+    const fileExt = path.extname(filePath);
+    console.log('File extension: ' + fileExt);
+    return fileExt;
+  };
+const fileExt = identifyFileExtension(convertedAbsolutePath);
+
+// 6)
+function readFile(filePath){
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    console.log(fileData);
+    return fileData;
+};
+const fileData = readFile(convertedAbsolutePath);
+
+function findLinksInFile(filePath) {
+    try {
+        filePath = fileData;
+      const linkRegex = /https?:\/\/[^\s]+/g;
+      const links = filePath.match(linkRegex);
+      console.log('Links found:', links);
+      return links;
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+  findLinksInFile(convertedAbsolutePath);
