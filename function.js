57639e0bf73f554db867af905c989eb333dfa860
Danitap-97 <131627619+Danitap-97@users.noreply.github.com>
2023-08-29 14:25:20 -0500

    uso función de fs para validar si existe el archivo, además verifico si la ruta es absoluta o relativa

--- function.js
+++ function.js
@@ -1,6 +1,60 @@
 //aqui realizo funciones pequeñas
-function validarRuta(router){
-return true;
-}
-console.log(validarRuta());
+const fs =require('fs');
+const path =require('node:path');
 
+
+const validarArchivos = function() {
+// verificar si el archivo existe.
+if(fs.existsSync('./pruebas/README2.md')){
+  console.log('El archivo EXISTE');
+}else{
+  console.log('El archivo NO EXISTE');
+}
+/* fs.stat('./pruebas/README2.md', (err, stats) => {
+  if (err) {
+      console.log("El archivo NO EXISTE!");
+  } else {
+      console.log("El archivo EXISTE!");
+    /*   console.log("Path is file:", stats.isFile());
+    console.log("Path is directory:", stats.isDirectory()); */
+  };
+  validarArchivos();
+
+//validar si es absoluta o relativa
+const ruta = 'README2.md';
+console.log('Ruta:', ruta);
+
+if (path.isAbsolute(ruta)) {
+ console.log('La ruta es absoluta.');
+ //verificar si es archivo o carpeta
+/*  */
+ 
+ //leo el contenido del archivo
+/* fs.readFile('README2.md', 'utf-8', (err, data) => {
+  if(err) {
+    console.log('error: ', err);
+  } else {
+    console.log(data);
+  }
+}); */
+ 
+} else {
+  const rutaAbsoluta = path.resolve(ruta);
+    console.log('La ruta es relativa.');
+}
+
+//validarArchivos();
+const validateFile = function (ruta){
+  return fs.existsSync(ruta);
+}
+const isAbsolute = function (ruta){
+ return path.isAbsolute(ruta);
+}
+const converAbsolute = function (ruta){
+  return path.resolve(ruta);
+}
+module.exports = {
+  validateFile,
+  isAbsolute,
+  converAbsolute,
+};
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-//aqui registro funciones de md-link
-const fs =require('fs').promises;
-const path = require('path');
-//const{
-  //aqui van las funciones
-//}=require(./function.js)
+//aqui registro funciones de md-link llamo las funciones
+/* const axios =require('axios');
+const  markdowLinkExtractor= require('node_modules/markdown-link-extractor');
+const fetch =require('node:fetch'); */
+
+const{
+  validateFile,
+  isAbsolute,
+  converAbsolute,
+}=require('./function.js')
+
+//aqui llamo mis funciones de function en orden y traigo su logica
+const main = function(){
+const ruta = './pruebas/README2.md'
+const exist = validateFile(ruta);
 
 
-module.exports = () => {
-  // ...
-};
+const absolute= isAbsolute(ruta);
+if(absolute);
+
+const conver = converAbsolute(ruta);
+
+
+}
+main();
