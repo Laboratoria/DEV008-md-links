a85a0612c87b50570efeb2b48ea5b458821e01d3
CarolynYQ <carolynyaker@gmail.com>
2023-09-01 03:30:55 -0500

    test función identifyFile

--- test/config.spec.js
+++ test/config.spec.js
@@ -2,13 +2,13 @@ const config = require("../src/config");
 
 describe("configuration of auxiliary functions", () => {
 
-  it("The path must exist ", () => {
+  test("the path must exist ", () => {
     const response = config.pathExist('src/sample/folderA/folderA.1/secondfile.md')
     expect(response).toBe(true);
     
   });
 
-  it("should return a absolute path", () => {
+  test("should return a absolute path", () => {
     const expectedResponse = 'C:\\Users\\kingk\\Laboratoria\\MD-links\\src\\sample';
     const pathExample = 'src/sample';
     const response = config.absolutePathConverter(pathExample)
@@ -16,11 +16,18 @@ describe("configuration of auxiliary functions", () => {
     
   });
 
-  it("should return array files", () => {
+  test("should return an array with a file", () => {
+    const expectedResponse = ['src/sample/folderA/folderA.1/secondfile.md'];
+    const response = config.identifyFile('src/sample/folderA/folderA.1/secondfile.md');
+    expect(response).toStrictEqual(expectedResponse);
+    
+  });
+
+  test("should return array files", () => {
     const expectedResponse = [
-        'C:\\Users\\kingk\\Laboratoria\\MD-links\\src\\sample\\folderA\\folderA.1\\folderA.1.1\\firstfile.md',
-        'C:\\Users\\kingk\\Laboratoria\\MD-links\\src\\sample\\folderA\\folderA.1\\secondfile.md',
-];
+      'C:\\Users\\kingk\\Laboratoria\\MD-links\\src\\sample\\folderA\\folderA.1\\folderA.1.1\\firstfile.md',
+      'C:\\Users\\kingk\\Laboratoria\\MD-links\\src\\sample\\folderA\\folderA.1\\secondfile.md',
+    ];
     const pathExample = 'src/sample/folderA';
     const response = config.scanDirectories(pathExample)
     expect(response).toStrictEqual(expectedResponse);
