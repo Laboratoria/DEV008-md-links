79bde4107192ea5c3a7d46e9ac500dbc09af4b44
Danitap-97 <131627619+Danitap-97@users.noreply.github.com>
2023-09-06 15:29:42 -0500

    se crea funcion para obtener el status y el file de los link

--- function.js
+++ function.js
@@ -1,47 +1,82 @@
 //aqui realizo funciones pequeñas
-const fs =require('fs');
-const path =require('node:path');
-const markdownLinkExtractor = require('markdown-link-extractor');
-const axios = require('axios');
+const fs = require("fs");
+const path = require("node:path");
+const markdownLinkExtractor = require("markdown-link-extractor");
+const axios = require("axios");
 
 //validarArchivos();
-const validateFile = function (ruta){
+const validateFile = function (ruta) {
   return fs.existsSync(ruta);
-}
-const isAbsolute = function (ruta){
- return path.isAbsolute(ruta);
-}
-const converAbsolute = function (ruta){
+};
+const isAbsolute = function (ruta) {
+  return path.isAbsolute(ruta);
+};
+const converAbsolute = function (ruta) {
   return path.resolve(ruta);
-}
- const isFile = function(ruta){
-  return fs.statSync(ruta).isFile() 
-}
+};
+const isFile = function (ruta) {
+  return fs.statSync(ruta).isFile();
+};
 console.log(isFile);
-const read = function(ruta){
-  return fs.readFileSync(ruta, 'utf-8', (err, data) => {
-    if(err) {
-      console.log('error: ', err);
+const read = function (ruta) {
+  return fs.readFileSync(ruta, "utf-8", (err, data) => {
+    if (err) {
+      console.log("error: ", err);
     } else {
       console.log(data);
     }
   });
-}
+};
 
-const getLink = function(ruta){
+const getLink = function (ruta) {
   const readIng = read(ruta);
-  const  objectLinks  = markdownLinkExtractor(readIng, true);
+  const objectLinks = markdownLinkExtractor(readIng, true);
   let arrayLinks = objectLinks.links;
-  const transformedLinks = arrayLinks.map((link) => (
-    {
-      href: link.href,
-      text: link.text, 
-      file: ruta,
-    }  
-  ));
+  const transformedLinks = arrayLinks.map((link) => ({
+    href: link.href,
+    text: link.text,
+    file: ruta,
+  }));
   return transformedLinks;
-}
+};
 
+const getPromisesHrefArray = (links) => {
+  let arrayPromiseArray = [];
+  links.forEach((link) => {
+    arrayPromiseArray = [...arrayPromiseArray, fetch(link.href)];
+  });
+  return Promise.allSettled(arrayPromiseArray);
+};
+
+const check = function (links, options, resolve) {
+  if (options !== undefined && typeof options === "object") {
+    if (options.validate && options.stats) {
+      resolve("tiene validate y stats");
+    } else if (options.stats) {
+      resolve("tienr stats");
+    } else if (options.validate) {
+      getPromisesHrefArray(links).then((results) => {
+        const  newlinks = links.map((link, linkIndex) => {
+          const result = results[linkIndex];
+          if (result.status === 'rejected') {
+            const statusCode = 'Mayor 200';
+            return {...link, status: statusCode, ok: 'fail'}
+          } else {
+            const statusCode = result.value.status;
+            return {...link, status: statusCode, ok: 'ok'}
+          }
+        });
+        resolve(newlinks);
+      });
+    } else if (options.validate === false || options.stats === false) {
+      resolve(links);
+    } else {
+      resolve("tiene opciones no requeridas");
+    }
+  } else {
+    resolve(links);
+  }
+};
 module.exports = {
   validateFile,
   isAbsolute,
@@ -49,4 +84,5 @@ module.exports = {
   isFile,
   read,
   getLink,
+  check,
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* const axios =require('axios');
 const  markdowLinkExtractor= require('node_modules/markdown-link-extractor');
 */
-const Path = require('node:path');
+const Path = require("node:path");
 /* const fetch = require('node-fetch'); */
 
 const {
@@ -11,56 +11,63 @@ const {
   converAbsolute,
   isFile,
   getLink,
+  check,
 } = require("./function.js");
 const { getFilesWithRecursively } = require("./api.js");
 
 //aqui llamo mis funciones de function en orden y traigo su logica
-const mdLinks  = (path, options) => new Promise((resolve, reject) => {
-  const exist = validateFile(path);
-  let links = [];
-  let rutaCalculada = "";
-  if (exist) {
-    const absolute = isAbsolute(path);
-    if (absolute) {
-      console.log("es absoluta");
-      rutaCalculada = path;
-    } else {
-      console.log("no es absoluta");
-      const conver = converAbsolute(path);
-      rutaCalculada = conver;
-      console.log(conver);
-    }
-    const file = isFile(rutaCalculada);
-    if (file) {
-      const extension = Path.extname(rutaCalculada);
-      if (extension === '.md') {
-        console.log("es un archivo");
-        links = [...links, ...getLink(rutaCalculada)];
-        resolve(links);
-      }else {
-        console.error('No es un archivo md')
+const mdLinks = (path, options) =>
+  new Promise((resolve, reject) => {
+    const exist = validateFile(path);
+    let links = [];
+    let rutaCalculada = "";
+    if (exist) {
+      const absolute = isAbsolute(path);
+      if (absolute) {
+        console.log("es absoluta");
+        rutaCalculada = path;
+      } else {
+        console.log("no es absoluta");
+        const conver = converAbsolute(path);
+        rutaCalculada = conver;
+        console.log(conver);
+      }
+      const file = isFile(rutaCalculada);
+      if (file) {
+        const extension = Path.extname(rutaCalculada);
+        if (extension === ".md") {
+          console.log("es un archivo");
+          links = [...links, ...getLink(rutaCalculada)];
+          check(links, options, resolve);
+        } else {
+          console.error("No es un archivo md");
+        }
+      } else {
+        const readFile = getFilesWithRecursively(rutaCalculada);
+        console.log(readFile, "readfile");
+        // todas las rutas de los archivos en un array
+        // iterar cada una de las rutas y obtener sus links
+        readFile.forEach((pathFile) => {
+          console.log(pathFile, "rutica");
+          links = [...links, ...getLink(pathFile)];
+        });
+        check(links, options, resolve);
       }
     } else {
-      const readFile = getFilesWithRecursively(rutaCalculada);
-      console.log(readFile, 'readfile');
-      // todas las rutas de los archivos en un array
-      // iterar cada una de las rutas y obtener sus links
-      readFile.forEach((pathFile)=>{
-        console.log(pathFile, 'rutica');
-        links = [...links, ...getLink(pathFile)];
-      })
-      resolve(links);
+      reject(Error("no existe la ruta"));
     }
-  } else {
-    reject(Error("no existe la ruta"));
-  }
-  
-});
+  });
 
-mdLinks("./pruebas/folder2/README4.md")
-  .then(links => {
+// mdLinks("./pruebas/folder2/README4.md")
+//   .then((links) => {
+//     console.log(links);
+//   })
+//   .catch(console.error);
+
+  mdLinks("./pruebas/folder2/README4.md", { validate: true })
+  .then((links) => {
     console.log(links);
   })
   .catch(console.error);
 
-exports.module = {}
+exports.module = {};
